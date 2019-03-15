ee1b6cbf235e2d648f8e785ce5d5b63fe861b235
Gabriel Hooks <gabriel.i.hooks@gmail.com>
2019-03-15 09:36:50 -0400

    Created character.py but idk what your variables mean.

---
+++ character.py
@@ -0,0 +1,4 @@
+# This file defines a character. So, instead of typing the same five lines over and over with diffent variables, you can just make a Character object and keep everything contained and neat.
+
+class Character :
+    # Idk what the variables really mean right now.
